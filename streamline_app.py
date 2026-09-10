947c298a2a8c80acac8cac4b292f7689da1372ae
Kanishka <167022958+d-kanishka@users.noreply.github.com>
2026-09-11 03:47:11 +0530

    Enhance fruit options fetching with SEARCH_ON column
    Updated the data fetching process to include the SEARCH_ON column from Snowflake and converted the Snowpark DataFrame to a Pandas DataFrame for further processing.

--- streamline_app.py
+++ streamline_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import pandas as pd
 import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
@@ -14,8 +15,11 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# Fetch available fruit options from Snowflake
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).collect()
+# Fetch available fruit options including the SEARCH_ON column from Snowflake
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
 
 # Multiselect widget with a max limit of 5 fruits
 ingredients_list = st.multiselect(
@@ -27,9 +31,13 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string = ''
 
-    # Build ingredient string and fetch specific fruit data from API
+    # Build ingredient string and fetch specific fruit data using the SEARCH_ON value
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
