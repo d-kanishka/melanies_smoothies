77ad35ce056d286d2f2c93e95b125cb1f08b682b
Kanishka <167022958+d-kanishka@users.noreply.github.com>
2026-09-11 03:34:02 +0530

    Fetch and display watermelon nutrition information
    Added functionality to display nutrition information for watermelon smoothie stage using an external API.

--- streamline_app.py
+++ streamline_app.py
@@ -2,6 +2,14 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+# New section to display smoothie stage nutrition information
+import requests
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
